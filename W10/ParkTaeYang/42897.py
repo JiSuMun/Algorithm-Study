7a77ce645b28f321b77d6e95fbd61e93a0dd1a7b
pty9714 <pty9714@naver.com>
2023-04-25 12:16:42 +0900

    Add W10_ParkTaeYang

---
+++ W10/ParkTaeYang/42897.py
@@ -0,0 +1,16 @@
+def solution(money):
+    num = len(money)
+    dp1 = [0] * (num-1)
+    dp1[0] = money[0]
+    for i in range(1, num-1):
+        dp1[i] = max(dp1[i-1],dp1[i-2]+money[i])
+    dp2 = [0] * num
+    for i in range(1, num):
+        dp2[i] = max(dp2[i-1],dp2[i-2]+money[i])
+    answer = max(dp1[num-2],dp2[num-1])
+    return answer
+
+m = [1,2,3,1]
+print(solution(m))
+
+# 시간복잡도는 for문 순환 O(N)
